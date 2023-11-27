f8bf1c88407151dab920a15a165c53de6c0508bf
Prasad Poojary <prasadspoojar@gmail.com>
2023-11-27 23:18:28 +0530

    init and routing configuration

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
+import { BuslistComponent } from './components/buslist/buslist.component';
+import { SeatComponent } from './components/seat/seat.component';
+import { PreviewComponent } from './components/preview/preview.component';
+import { TicketComponent } from './components/ticket/ticket.component';
+import { UserComponent } from './components/user/user.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SearchComponent,
+  },
+  {
+    path: 'bus/:from/:to',
+    component: BuslistComponent,
+  },
+  {
+    path: 'seat/:busId',
+    component: SeatComponent,
+  },
+  {
+    path: 'user/:ticketId',
+    component: UserComponent,
+  },
+  {
+    path: 'preview/:ticketId',
+    component: PreviewComponent,
+  },
+  {
+    path: 'ticket/:ticketId',
+    component: TicketComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { BuslistComponent } from './components/buslist/buslist.component';
+import { SeatComponent } from './components/seat/seat.component';
+import { UserComponent } from './components/user/user.component';
+import { PreviewComponent } from './components/preview/preview.component';
+import { TicketComponent } from './components/ticket/ticket.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    BuslistComponent,
+    SeatComponent,
+    UserComponent,
+    PreviewComponent,
+    TicketComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
